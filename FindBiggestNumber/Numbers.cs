85e547689ab9e5cc81ef5c91c0a6594859be8543
BuhleMcetywa <buhlemcetywa07@gmail.com>
2023-11-28 11:57:33 +0200

    First Commit

---
+++ FindBiggestNumber/Numbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindBiggestNumber
+{
+    internal class Numbers
+    {
+        public static int BiggestNumber(int[] numbers)
+        {
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+                if (numbers[i] > max)
+                    max = numbers[i];
+            return max;
+
+        }
+
+        public static int SecondBiggestNumber(int[] numbers)
+        {
+            int max = Math.Max(numbers[0], numbers[1]);
+            int result = Math.Min(numbers[0], numbers[1]);
+            for (int i = 2; i < numbers.Length; i++)
+                if (numbers[i] >= max)
+                    (result, max) = (max, numbers[i]);
+                else if (numbers[i] > result) 
+            result = numbers[i];
+
+            return result; 
+
+
+        }
+    }  
+}
+
+
---
+++ FindBiggestNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FindBiggestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers= { 2, 1, 7, 4, 9, 5 };
+           int biggest= Numbers.BiggestNumber(myNumbers);
+            int SecondBiggestNumber = Numbers.SecondBiggestNumber(myNumbers);
+            Console.WriteLine("The biggest number is " + biggest);
+            Console.WriteLine("The second biggest number is " + SecondBiggestNumber);
+            string capitalized = Words.CapitalizeFirstWords( "I hope you had a good weekend?");
+            Console.WriteLine(capitalized);
+        }
+    }
+}
